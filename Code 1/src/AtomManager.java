6625505986f60fc49eea6fa7e28c3c45564abc59
Abdullah Shinwari <abdullah.shinwari@ucdconnect.ie>
2024-03-04 20:27:55 +0000

    Fixed containsPair() and setter_generatingAtoms()

--- Code 1/src/AtomManager.java
+++ Code 1/src/AtomManager.java
@@ -12,19 +12,19 @@ public class AtomManager {
 
         public static void main(String[] args) {//Testing Purposes
 
-            AtomManager atomManager = new AtomManager();
+//            AtomManager atomManager = new AtomManager();
+//
+//            atomManager.createAtom(1, 2);
+//            atomManager.createAtom(3, 4);
+//
+//            List<Atom> allAtoms = atomManager.getAtoms();
+//
+//            for (Atom atom : allAtoms) {
+//                System.out.println(atom.toString());
+//            }
 
-            atomManager.createAtom(1, 2);
-            atomManager.createAtom(3, 4);
 
-            List<Atom> allAtoms = atomManager.getAtoms();
-
-            for (Atom atom : allAtoms) {
-                System.out.println(atom.toString());
-            }
-
-
-            //setter_generatingAtoms();
+            setter_generatingAtoms();
         }
 
     public void createAtom(int x, int y) {
@@ -32,18 +32,17 @@ public class AtomManager {
         atoms.add(atom);
     }
 
-    public static void setter_generatingAtoms() { //In Progress - Abdullah
+    public static void setter_generatingAtoms() {
         ArrayList<int[]> coordinates = new ArrayList<>();
         Random random = new Random();
         int count = 0;
 
         System.out.println("Generated pairs:");
 
-        // Continue generating pairs until we have 6 unique pairs
         while (count < 6) {
             //Hard coded numbers; CHANGE after board implementation.
             int firstNumber = random.nextInt(9);
-            int secondNumber = random.nextInt(10); //secondNumber would be the columns, and they can be negative too.
+            int secondNumber = random.nextInt(9) - 4; //secondNumber would be the columns, and they can be negative too.
 
             //Check if coordinates are in appropiate range and not Duplicates.
             if (Utility.inRange(firstNumber, secondNumber) && !Utility.containsPair(coordinates, firstNumber, secondNumber)) {
