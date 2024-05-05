6de4c20db377ea509a29b23c947982093c15ed2f
Abdullah Shinwari <abdullah.shinwari@ucdconnect.ie>
2024-05-05 19:48:34 +0100

    added javadoc comments to TrajectoryManager

--- _31_alwayslategroup/src/UTILITY/TrajectoryManager.java
+++ _31_alwayslategroup/src/UTILITY/TrajectoryManager.java
@@ -5,10 +5,22 @@ import MATH.Trajectory;
 
 import java.util.ArrayList;
 
+
+/**
+ * The TrajectoryManager class manages trajectories.
+ * Its functionality includes the creation of these possible trajectories
+ * and adds them the list of trajectories.
+
+ * This class also provides methods to retrieve the list of atoms created.
+ */
 public class TrajectoryManager {
 
     public ArrayList<Trajectory> trajectories;
 
+    /**
+     * Constructs TrajectoryManager object, initialising an empty list of trajectories
+     * and calls the setTrajectories() method to populate the list with predefined trajectories.
+     */
     public TrajectoryManager(){
         trajectories = new ArrayList<>();
         setTrajectories();
@@ -19,6 +31,13 @@ public class TrajectoryManager {
         TrajectoryManager t = new TrajectoryManager();
     }
 
+    /**
+     * Generates a set of 6 trajectories possible in the hexagonal board and adds it to the list of trajectories.
+     * Each trajectory consists of three coordinates representing the movement in the forward, right, and left directions.
+     *
+     * Note: The implementation uses hard-coded values derived from observations of direction propagation on the board.
+     * These trajectories are essential for simulating the movement of rays across the board.
+     */
     public void setTrajectories(){
 
         //                           MATH.Trajectory(trajectory                 , right_direction            , left_direction             );
